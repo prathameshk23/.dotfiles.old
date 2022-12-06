4f1ab44cc1da88961b890db2efb1be5c42cf9564
Manan Karnik <manankarnik03@gmail.com>
2022-12-06 20:18:57 +0530

    add options

---
+++ .config/nvim/lua/user/options.lua
@@ -0,0 +1,31 @@
+local options = {
+  -- Indentation
+  tabstop = 2,
+  shiftwidth = 2,
+  expandtab = true,
+  smarttab = true,
+
+  -- Search
+  ignorecase = true,
+  smartcase = true,
+  hlsearch = false,
+
+  -- User Interface
+  splitbelow = true,
+  splitright = true,
+  relativenumber = true,
+  cursorline = true,
+  title = true,
+
+  -- Text Rendering
+  scrolloff = 1,
+  wrap = true,
+  linebreak = true,
+
+  -- Misc
+  confirm = true,
+}
+
+for key, value in pairs(options) do
+  vim.opt[key] = value
+end
