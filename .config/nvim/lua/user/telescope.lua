925685eeca11ded9ac458407cef7eb4bb64c48cc
Manan Karnik <manankarnik03@gmail.com>
2022-12-18 12:32:39 +0530

    add exit keymap

--- .config/nvim/lua/user/telescope.lua
+++ .config/nvim/lua/user/telescope.lua
@@ -1,4 +1,15 @@
 local builtin = require('telescope.builtin')
+local actions = require("telescope.actions")
+require("telescope").setup({
+  defaults = {
+    mappings = {
+      i = {
+        ["<ESC>"] = actions.close,
+      },
+    },
+  },
+})
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
